22cde62e7923bed6afab36a74fe03de978f7fd97
Rodrinardi <rodrigobernardi1975@gmail.com>
2023-08-30 22:25:11 -0300

    Exercicio 3
    Usuário digita o nome e a idade

---
+++ Aula6-Funções/ex3.dart
@@ -0,0 +1,16 @@
+import "dart:io";
+void exibe_msg(String nome, int idade)
+{
+  print("Nome: $nome, idade: $idade");
+}
+
+void main()
+{
+  String nome;
+  int idade;
+  print("Digite seu nome");
+  nome = stdin.readLineSync()!;
+  print("Digite a sua idade");
+  idade = int.parse(stdin.readLineSync()!);
+  exibe_msg(nome, idade);
+}
